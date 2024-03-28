608658792ffde0de982008adda9e016c9247b842
Kevin Huang <41244296+kh9sd@users.noreply.github.com>
2024-03-27 21:38:29 -0400

    starting impl of calibration log

--- calibrator.py
+++ calibrator.py
@@ -60,24 +60,34 @@ class LoggerApp:
         left_frame  = Frame(self.master,  width=200,  height=  400)
         left_frame.grid(row=0, column=0, sticky="N")
 
-        self.address_label = Label(left_frame, text="Address")
+        self.address_label = Label(left_frame, text="JSON index:")
         self.address_var = StringVar(value='')
         self.address_entry = Entry(left_frame, textvariable=self.address_var, width=100)
         self.info_bar = Label(left_frame, text=ERRORLESS_INFO, fg="black")
 
+        self.calibrated_input_var = StringVar(value='N/A')
+        self.calibrated_input_entry = Entry(left_frame, textvariable=self.calibrated_input_var)
+        self.log_button = Button(left_frame,text="Log calibration", command=self.update_shit)
+        self.display_var = StringVar(value='')
+        self.display_label = Entry(left_frame, textvariable=self.display_var)
+        self.display_label["state"] = "readonly"
+
         self.address_label.grid(row=1, column=0, sticky="N")
         self.address_entry.grid(row=1, column=1, columnspan=2, sticky="N")
-        self.info_bar.grid(row=2, column=0)
+        self.info_bar.grid(row=2, column=1)
+        self.calibrated_input_entry.grid(row=3, column=0)
+        self.log_button.grid(row=4, column=0)
+        self.display_label.grid(row=5, column=0)
 
         # make layout a little nicer
-        self.blankspace = Label(left_frame, text="")
-        self.blankspace_2 = Label(left_frame, text="")
-        self.blankspace_3 = Label(left_frame, text="")
-        self.blankspace_4 = Label(left_frame, text="")
-        self.blankspace.grid(row=0,column=0,columnspan=3)
-        self.blankspace_2.grid(row=2,column=0,columnspan=3)
-        self.blankspace_3.grid(row=6,column=0,columnspan=3)
-        self.blankspace_4.grid(row=4, column=0, columnspan=3)
+        # self.blankspace = Label(left_frame, text="")
+        # self.blankspace_2 = Label(left_frame, text="")
+        # self.blankspace_3 = Label(left_frame, text="")
+        # self.blankspace_4 = Label(left_frame, text="")
+        # self.blankspace.grid(row=0,column=0,columnspan=3)
+        # self.blankspace_2.grid(row=2,column=0,columnspan=3)
+        # self.blankspace_3.grid(row=6,column=0,columnspan=3)
+        # self.blankspace_4.grid(row=4, column=0, columnspan=3)
 
 
 
@@ -93,10 +103,9 @@ class LoggerApp:
         self.canvas.configure(yscrollcommand=scrolly.set)
         self.canvas.grid(row=0, column=0)
 
-
-
         self.last_gui_update_time = 0
-        self.cur_data_json = json.loads(EXAMPLE_DATA_JSON)
+        self.newest_fresh_json_data = json.loads(EXAMPLE_DATA_JSON)
+        self.cur_selected_json = self.newest_fresh_json_data
 
         self.worker_thread = threading.Thread(target=self.start_websocket, daemon=True)
         self.worker_thread.start()
@@ -104,12 +113,16 @@ class LoggerApp:
         self.main_loop()
 
 
+    def update_shit(self):
+        self.display_var.set(f"{self.display_var.get()}\n{self.calibrated_input_entry.get()}, {self.cur_selected_json}")
+
+
     def main_loop(self):
         user_str = self.address_var.get()
 
-        json_to_be_displayed = self.cur_data_json # set to default og json if user input doesn't work
+        json_to_be_displayed = self.newest_fresh_json_data # set to default og json if user input doesn't work
         try:
-            json_to_be_displayed = eval(f"self.cur_data_json{user_str}")
+            json_to_be_displayed = eval(f"self.newest_fresh_json_data{user_str}")
             self.info_bar.config(fg="black", text=ERRORLESS_INFO)
         except SyntaxError as e:
             self.info_bar.config(fg="red", text=repr(e))
@@ -118,8 +131,9 @@ class LoggerApp:
         except Exception as e:
             self.info_bar.config(fg="red", text=repr(e))
 
+        self.cur_selected_json = json.dumps(json_to_be_displayed, indent=4)
         self.canvas.delete("all") # rerender the new text
-        self.canvas.create_text(10,0,text=json.dumps(json_to_be_displayed, indent=4), anchor="nw", width=700)
+        self.canvas.create_text(10,0,text=self.cur_selected_json, anchor="nw", width=700)
         self.canvas.configure(scrollregion=self.canvas.bbox("all"))
         self.master.after(1000, self.main_loop)
 
@@ -141,7 +155,7 @@ class LoggerApp:
     # This is where we get handle the ECS json reports. You'll want to update this if you start getting interesting info other than the items (e.g., loadcellsensors, aborts, etc.) below
     def process_data(self, json_data):
         if json_data['command'] == 'DATA':
-            self.cur_data_json = json_data
+            self.newest_fresh_json_data = json_data
 
         # only upate the GUI with json report count at 2Hz (time.time is in *seconds* not ms)
         # self.file_count += 1
